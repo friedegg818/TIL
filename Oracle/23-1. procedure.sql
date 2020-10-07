da9611073fcdaa3d5b4b677c44b732a1a7188d30
SSONG <65526381+friedegg818@users.noreply.github.com>
2020-10-08 00:35:52 +0900

    Update 23-1. procedure.sql

--- Oracle/23-1. procedure.sql
+++ Oracle/23-1. procedure.sql
@@ -1,109 +1,106 @@
-<Member ÀúÀå ÇÁ·Î½ÃÀú ÀÛ¼º> 
 
-- Ãß°¡ ÇÁ·Î½ÃÀú
-CREATE OR REPLACE PROCEDURE insertMember
-(
-    pId member1.id%TYPE,
-    pPwd member1.pwd%TYPE,
-    pName member1.name%TYPE,
-    pBirth member2.birth%TYPE,
-    pEmail member2.email%TYPE,
-    pTel member2.tel%TYPE
-)
-IS
-BEGIN 
-    INSERT INTO member1 (id, pwd, name) VALUES (pId, pPwd, pName); 
-    INSERT INTO member2 (id, birth, email, tel) VALUES (pId, pBirth, pEmail, pTel);
-    COMMIT;
-END;
-/
+<Member ì €ìž¥ í”„ë¡œì‹œì € ìž‘ì„±> 
 
-SELECT * FROM member1;
-SELECT * FROM member2;
+    - ì¶”ê°€ í”„ë¡œì‹œì €
+        CREATE OR REPLACE PROCEDURE insertMember
+        (
+            pId member1.id%TYPE,
+            pPwd member1.pwd%TYPE,
+            pName member1.name%TYPE,
+            pBirth member2.birth%TYPE,
+            pEmail member2.email%TYPE,
+            pTel member2.tel%TYPE
+        )
+        IS
+        BEGIN 
+            INSERT INTO member1 (id, pwd, name) VALUES (pId, pPwd, pName); 
+            INSERT INTO member2 (id, birth, email, tel) VALUES (pId, pBirth, pEmail, pTel);
+            COMMIT;
+        END;
+        /
 
-- ¼öÁ¤ ÇÁ·Î½ÃÀú
-CREATE OR REPLACE PROCEDURE updateMember
-(
-    pId member1.id%TYPE,
-    pPwd member1.pwd%TYPE,
-    pName member1.name%TYPE,
-    pBirth member2.birth%TYPE,
-    pEmail member2.email%TYPE,
-    pTel member2.tel%TYPE
-    
-)
-IS
-BEGIN 
-    UPDATE member1 SET pwd = pPwd, name = pName WHERE id = pId; 
-    UPDATE member2 SET birth = pBirth, email = pEmail, tel = pTel WHERE id = pId;
-    COMMIT;
-END;
-/
+        SELECT * FROM member1;
+        SELECT * FROM member2;
 
-- »èÁ¦ ÇÁ·Î½ÃÀú 
-CREATE OR REPLACE PROCEDURE deleteMember 
-(
-    pId member1.id%TYPE
-)
-IS
-BEGIN
-    DELETE FROM member2 WHERE id = pId;
-    DELETE FROM member1 WHERE id = pId;
-    IF SQL%NOTFOUND THEN 
-        RAISE_APPLICATION_ERROR(-20100, 'µî·ÏµÈ ÀÚ·á°¡ ¾Æ´Õ´Ï´Ù.'); 
-    END IF;
-    COMMIT;
-END;
-/
+    - ìˆ˜ì • í”„ë¡œì‹œì €
+        CREATE OR REPLACE PROCEDURE updateMember
+        (
+            pId member1.id%TYPE,
+            pPwd member1.pwd%TYPE,
+            pName member1.name%TYPE,
+            pBirth member2.birth%TYPE,
+            pEmail member2.email%TYPE,
+            pTel member2.tel%TYPE
 
+        )
+        IS
+        BEGIN 
+            UPDATE member1 SET pwd = pPwd, name = pName WHERE id = pId; 
+            UPDATE member2 SET birth = pBirth, email = pEmail, tel = pTel WHERE id = pId;
+            COMMIT;
+        END;
+        /
 
+    - ì‚­ì œ í”„ë¡œì‹œì € 
+        CREATE OR REPLACE PROCEDURE deleteMember 
+        (
+            pId member1.id%TYPE
+        )
+        IS
+        BEGIN
+            DELETE FROM member2 WHERE id = pId;
+            DELETE FROM member1 WHERE id = pId;
+            IF SQL%NOTFOUND THEN 
+                RAISE_APPLICATION_ERROR(-20100, 'ë“±ë¡ëœ ìžë£Œê°€ ì•„ë‹™ë‹ˆë‹¤.'); 
+            END IF;
+            COMMIT;
+        END;
+        /
 
+    - ì•„ì´ë”” ê²€ìƒ‰ í”„ë¡œì‹œì €
 
-- ¾ÆÀÌµð °Ë»ö ÇÁ·Î½ÃÀú
-
-CREATE OR REPLACE PROCEDURE findByIdMember
-(
-    pResult OUT SYS_REFCURSOR,
-    pId IN VARCHAR2
-)
-IS
-BEGIN
-    OPEN pResult FOR
-        SELECT m1.id, name, birth, email, tel 
-        FROM member1 m1
-        JOIN member2 m2 ON m1.id = m2.id
-        WHERE m1.id = pId;
-END;
-/
+        CREATE OR REPLACE PROCEDURE findByIdMember
+        (
+            pResult OUT SYS_REFCURSOR,
+            pId IN VARCHAR2
+        )
+        IS
+        BEGIN
+            OPEN pResult FOR
+                SELECT m1.id, name, birth, email, tel 
+                FROM member1 m1
+                JOIN member2 m2 ON m1.id = m2.id
+                WHERE m1.id = pId;
+        END;
+        /
 
 
-- ÀüÃ¼ Ãâ·Â ÇÁ·Î½ÃÀú
+    - ì „ì²´ ì¶œë ¥ í”„ë¡œì‹œì €
+        CREATE OR REPLACE PROCEDURE listMember
+        (
+            pResult OUT SYS_REFCURSOR
+        )
+        IS
+        BEGIN
+            OPEN pResult FOR
+                SELECT m1.id, name, birth, email, tel 
+                FROM member1 m1
+                JOIN member2 m2 ON m1.id = m2.id;
+        END;
+        /
 
-CREATE OR REPLACE PROCEDURE listMember
-(
-    pResult OUT SYS_REFCURSOR
-)
-IS
-BEGIN
-    OPEN pResult FOR
-        SELECT m1.id, name, birth, email, tel 
-        FROM member1 m1
-        JOIN member2 m2 ON m1.id = m2.id;
-END;
-/
-
-- ÀÌ¸§ °Ë»ö ÇÁ·Î½ÃÀú 
-CREATE OR REPLACE PROCEDURE findByNameMember
-(
-    pResult OUT SYS_REFCURSOR,
-    pName IN VARCHAR2
-)
-IS
-BEGIN
-    OPEN pResult FOR
-        SELECT m1.id, name, birth, email, tel 
-        FROM member1 m1
-        JOIN member2 m2 ON m1.id = m2.id
-        WHERE INSTR(m1.name, pName) > = 1;
-END;
-/
+    - ì´ë¦„ ê²€ìƒ‰ í”„ë¡œì‹œì € 
+        CREATE OR REPLACE PROCEDURE findByNameMember
+        (
+            pResult OUT SYS_REFCURSOR,
+            pName IN VARCHAR2
+        )
+        IS
+        BEGIN
+            OPEN pResult FOR
+                SELECT m1.id, name, birth, email, tel 
+                FROM member1 m1
+                JOIN member2 m2 ON m1.id = m2.id
+                WHERE INSTR(m1.name, pName) > = 1;
+        END;
+        /
